8e54cd2c172574cba7d35dc8c7f03c2062cc65d6
DiPi <dhanpatineupane@gmail.com>
2020-04-11 22:45:07 +0200

    first commit

---
+++ src/main/java/com/dipi/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.dipi.springdemo.service;
+
+import java.util.List;
+
+import com.dipi.springdemo.model.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
